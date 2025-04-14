5a2ac4ebf336c8b1c543ce6ebb177882c66e22fd
ConnorC04 <connorjcasey@iCloud.com>
2025-04-14 15:18:23 -0400

    rory green jacket btw

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,9 +1,12 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -15,7 +18,33 @@ public class GroceryReporter {
     @Override
     public String toString() {
 
+        GroceryReporter reporter = new GroceryReporter(originalFileText);
+        ItemParser parser = new ItemParser();
+
+        List<Item> items = parser.parseItemList(originalFileText);
+
+        Map<Item, Integer> itemCount = new HashMap<>();
+        Map<Item, Map<Double, Integer>> priceCount = new HashMap<>();
         int errorCount = 0;
+
+        for (Item item : items){
+            itemCount.put(item, itemCount.getOrDefault(item, 0) + 1);
+            priceCount.putIfAbsent(item, new HashMap<>());
+            priceCount.get(item).put(item.getPrice(), priceCount.get(item).getOrDefault(item.getPrice(), 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Item item : items){
+            sb.append("name:    ").append(item.getName()).append(" \t seen: ").append(itemCount.get(item)).append(" times\n");
+            sb.append("=============  \t =============\n");
+            Map<Double, Integer> prices = priceCount.get(item);
+            for (Double price : prices.keySet()) {
+                sb.append("Price:     ").append(price).append(" \t seen: ").append(prices.get(price)).append(" times\n");
+                sb.append("------------- \t -------------\n");
+            }
+            sb.append("\n");
+        }
+
 
         return null;
     }
